88c40e91418e0d70dbf469c253aff67694e280a6
Rui Laite <110286868+R-light07@users.noreply.github.com>
2026-04-27 21:36:41 +0200

    commit

--- script.js
+++ script.js
@@ -849,3 +849,7 @@ document.addEventListener('DOMContentLoaded', () => {
   initSmoothScroll();
   initScrollReveal();
 });
+
+document.getElementById("dashboardBtn").addEventListener("click", function () {
+  window.location.href = "./admin/index.html";
+});
